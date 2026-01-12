b45625e6454769ad6bf4bfa61302623ec99ca469
Alex Maksimov <maleksware@gmail.com>
2026-01-12 11:25:21 +1100

    Implement a lambda expression parser

---
+++ Lambda.hs
@@ -0,0 +1,98 @@
+module Lambda where
+
+import Data.Char
+import Control.Applicative
+
+type Variable = String
+
+data LExpr = Var Variable
+           | App LExpr LExpr
+           | Abs Variable LExpr
+  deriving (Eq, Show)
+
+newtype Parser a = Parser {
+  runParser :: String -> Maybe (a, String)
+}
+
+-- Easier to type
+lambda :: Char
+lambda = '&'
+
+instance Functor Parser where
+  fmap f (Parser p) = Parser $ \inp -> do
+    (r, rest) <- p inp
+    return (f r, rest)
+
+instance Applicative Parser where
+  pure c = Parser $ \inp -> Just (c, inp)
+  Parser f <*> Parser p = Parser $ \inp -> do
+    (fn, rest) <- f inp
+    (p', rest') <- p rest
+    return (fn p', rest')
+
+instance Alternative Parser where
+  empty = Parser (const Nothing)
+  Parser u <|> Parser v = Parser $ \inp -> u inp <|> v inp
+
+instance Monad Parser where
+  return = pure
+  (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+  Parser p >>= f = Parser $ \inp -> case p inp of
+    Just (r, rest) -> let (Parser f') = f r in f' inp
+    Nothing -> Nothing
+
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy pred = Parser $ \inp ->
+  case inp of
+    x:rest | pred x-> Just (x, rest)
+           | otherwise -> Nothing
+    _      -> Nothing
+
+charP :: Char -> Parser Char
+charP c = satisfy (==c)
+
+-- It is unclear how to efficiently parse trailing separators as opposed to leading.
+-- sepBy p sp = many (p <* sp) <|> (++) <$> many (p <* sp) <*> (pure <$> p) <|> pure []
+-- is an implementation that should do the trick, but it is inefficient in many ways
+-- (the most obvious being the ++)
+sepBy :: Parser a -> Parser b -> Parser [a]
+sepBy p sp = (:) <$> p <*> many (sp *> p) <|> pure []
+
+stringP :: String -> Parser String
+stringP = traverse (\c -> satisfy (==c))
+
+term :: Parser LExpr
+term = variable <|> abstraction <|> grouping
+
+ws :: Parser String
+ws = many (satisfy isSpace)
+
+literal :: Parser Variable
+literal = Parser $ \inp ->
+  let (xs, rest) = span (\c -> isDigit c || isAlpha c) inp
+  in (if null xs then Nothing else Just (xs, rest))
+
+variable :: Parser LExpr
+variable = Var <$> literal
+
+abstraction :: Parser LExpr
+abstraction = (\_ v _ e -> Abs v e) <$> charP lambda <* ws
+                                    <*> literal <* ws
+                                    <*> charP '.' <* ws
+                                    <*> expression
+
+application :: Parser LExpr
+application = foldl1 App <$> sepBy term (some $ satisfy isSpace)
+
+grouping :: Parser LExpr
+grouping = charP '(' *> expression <* charP ')'
+
+expression :: Parser LExpr
+expression = application <|> term
+
+formatExpr :: LExpr -> String
+formatExpr expr = case expr of
+  Var v -> v
+  Abs v rest -> "& " ++ v ++ ".[" ++ formatExpr rest ++ "]"
+  App f a -> "({" ++ formatExpr f ++ "} {" ++ formatExpr a ++ "})"
+
--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 Parser combinators and Pratt parsing (TBA).
 
 `ParserTsoding.hs` parses JSON (with minor omissions from the standard like doubles and string escaping) and is practically based on [this video by Tsoding](https://www.youtube.com/watch?v=N9RUqGYuGfw). Some implementations differ. Unlike the original, this parser supports negative numbers. Note that this parser is not (yet) a `Monad`.
+
+`Lambda.hs` parses lambda expressions. It's worth noting that the common "grammar" for the language, namely
+
+```
+M ::= V | \x.M | M M
+```
+
+or its slight variation
+
+
+```
+M ::= V | \x.M | (M M)
+```
+
+that can be found online is not suitable for robust parsing. The first grammar is even worse than the second because it's nontrivial to parse with a left recursive parser (which combinators, at least with unrefactored terms, mostly are). The second option is better in the sense that it seems nonambiguous, but most lambda terms out in the wild will assume left associativity of application and omit parentheses where possible.
+
+De Brujin parsing for lambda expressions is TBA.
